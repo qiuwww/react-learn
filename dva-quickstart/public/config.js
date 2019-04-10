5b91cf105febd904018a43e7da5129e53d262fe4
qiuweiwei <weiwei.qiu@beitu-inc.com>
2019-04-10 11:52:59 +0800

    update

--- dva-quickstart/public/config.js
+++
@@ -1,5 +0,0 @@
-// 商户的应用名称
-window.product_name = "招财宝";
-window.ORIGIN = "http://47.110.211.87:3042";
-// 这个是后台的地址
-window.adminOrigin = "http://47.110.211.87:3043";
--- dva-quickstart/src/services/overdue.js
+++
@@ -1,5 +0,0 @@
-import { fetchPost } from '../utils/request';
-
-export async function queryExportRecord(params) {// 导出前记录
-  return fetchPost('/user/export/record', { ...params });
-}
